ef676d6c7976387a623800f74dbbbaa7bfe7fe87
Akshay-2396 <itsakshay.ak96@gmail.com>
2025-08-04 14:15:17 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,37 @@
+// import { createRoot } from "react-dom/client";
+// import App from "./App.jsx";
+// import "./index.css";
+// import { BrowserRouter } from "react-router-dom";
+// import { Provider } from "react-redux";
+// import store from "./store/store.js";
+// import { Toaster } from "./components/ui/toaster.jsx";
+
+// createRoot(document.getElementById("root")).render(
+//   <BrowserRouter>
+//     <Provider store={store}>
+//       <App />
+//       <Toaster />
+//     </Provider>
+//   </BrowserRouter>
+// );
+
+
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./store/store.js";
+import { store, persistor } from "./store/store.js"; // ✅ import persistor too
+import { PersistGate } from "redux-persist/integration/react";
 import { Toaster } from "./components/ui/toaster.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Provider store={store}>
-      <App />
-      <Toaster />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+        <Toaster />
+      </PersistGate>
     </Provider>
   </BrowserRouter>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,51 @@
-import { configureStore } from "@reduxjs/toolkit";
+// import { configureStore } from "@reduxjs/toolkit";
+// import authReducer from "./auth-slice";
+// import adminProductsSlice from "./admin/products-slice";
+// import adminOrderSlice from "./admin/order-slice";
+
+// import shopProductsSlice from "./shop/products-slice";
+// import shopCartSlice from "./shop/cart-slice";
+// import shopAddressSlice from "./shop/address-slice";
+// import shopOrderSlice from "./shop/order-slice";
+// import shopSearchSlice from "./shop/search-slice";
+// import shopReviewSlice from "./shop/review-slice";
+// import commonFeatureSlice from "./common-slice";
+
+// const store = configureStore({
+//   reducer: {
+//     auth: authReducer,
+
+//     adminProducts: adminProductsSlice,
+//     adminOrder: adminOrderSlice,
+
+//     shopProducts: shopProductsSlice,
+//     shopCart: shopCartSlice,
+//     shopAddress: shopAddressSlice,
+//     shopOrder: shopOrderSlice,
+//     shopSearch: shopSearchSlice,
+//     shopReview: shopReviewSlice,
+
+//     commonFeature: commonFeatureSlice,
+    
+//   },
+// });
+
+// export default store;
+
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage"; // uses localStorage
+
+// Import slices
 import authReducer from "./auth-slice";
 import adminProductsSlice from "./admin/products-slice";
 import adminOrderSlice from "./admin/order-slice";
@@ -11,23 +58,45 @@ import shopSearchSlice from "./shop/search-slice";
 import shopReviewSlice from "./shop/review-slice";
 import commonFeatureSlice from "./common-slice";
 
-const store = configureStore({
-  reducer: {
-    auth: authReducer,
+// Configure persistence
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["auth"], // only persist auth slice
+};
 
-    adminProducts: adminProductsSlice,
-    adminOrder: adminOrderSlice,
+// Combine all reducers
+const rootReducer = combineReducers({
+  auth: authReducer,
 
-    shopProducts: shopProductsSlice,
-    shopCart: shopCartSlice,
-    shopAddress: shopAddressSlice,
-    shopOrder: shopOrderSlice,
-    shopSearch: shopSearchSlice,
-    shopReview: shopReviewSlice,
+  adminProducts: adminProductsSlice,
+  adminOrder: adminOrderSlice,
 
-    commonFeature: commonFeatureSlice,
-    
-  },
+  shopProducts: shopProductsSlice,
+  shopCart: shopCartSlice,
+  shopAddress: shopAddressSlice,
+  shopOrder: shopOrderSlice,
+  shopSearch: shopSearchSlice,
+  shopReview: shopReviewSlice,
+
+  commonFeature: commonFeatureSlice,
 });
 
-export default store;
+// Persisted reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Store
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+// Persistor
+export const persistor = persistStore(store);
+
+

9d555a716a98fffc212a35d9aa2cdc4a8eec2913
Akshay-2396 <itsakshay.ak96@gmail.com>
2025-08-05 12:47:45 +0530

    modified

--- src/config/index.js
+++ src/config/index.js
@@ -244,7 +244,7 @@ export const addressFormControls = [
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL + "/api",
+  baseURL: import.meta.env.VITE_BACKEND_URL + "/shop",
   withCredentials: true,
 });
 

5dea9da180621e0bc9ee335fb0a96f6e3d093106
Akshay-2396 <itsakshay.ak96@gmail.com>
2025-08-06 17:45:29 +0530

    modified

--- src/config/index.js
+++ src/config/index.js
@@ -223,41 +223,6 @@ export const addressFormControls = [
   },
 ];
 
-// import axios from "axios";
-
-// const axiosInstance = axios.create({
-//   baseURL: "http://localhost:5000/api",
-//   withCredentials: true,
-// });
-
-// axiosInstance.interceptors.request.use((config) => {
-//   const token = localStorage.getItem("token");
-
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
-
-// export default axiosInstance;
-
-
-// import axios from "axios";
-
-// const axiosInstance = axios.create({
-//   baseURL: import.meta.env.VITE_BACKEND_URL ,
-//   withCredentials: true,
-// });
-
-// axiosInstance.interceptors.request.use((config) => {
-//   const token = localStorage.getItem("token");
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
-
-// export default axiosInstance;
 
 import axios from "axios";
 
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -22,26 +22,3 @@ export const addToWishlist = async (productId) => {
 export const removeFromWishlist = async (productId) => {
   await axiosInstance.delete(`/wishlist/${productId}`);
 };
-
-// import axios from "axios";
-
-// axios.defaults.baseURL = "http://localhost:5000/api";
-// axios.defaults.withCredentials = true;
-
-// // ✅ Fetch wishlist
-// export const fetchWishlist = async () => {
-//   const res = await axios.get("/wishlist");
-//   return res.data;
-// };
-
-// // ✅ Add to wishlist
-// export const addToWishlist = async (productId) => {
-//   const res = await axios.post("/wishlist", { productId });
-//   return res.data;
-// };
-
-// // ✅ Remove from wishlist
-// export const removeFromWishlist = async (productId) => {
-//   const res = await axios.delete(`/wishlist/${productId}`);
-//   return res.data;
-// };

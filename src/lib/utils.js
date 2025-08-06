d06a879c4120d9241b3641d8fe49e517f7ea3967
Akshay-2396 <itsakshay.ak96@gmail.com>
2025-08-06 11:08:34 +0530

    modified

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -23,67 +23,25 @@ export const removeFromWishlist = async (productId) => {
   await axiosInstance.delete(`/wishlist/${productId}`);
 };
 
-
-// // --- Axios Config ---
 // import axios from "axios";
 
-// const axiosInstance = axios.create({
-//   baseURL: "http://localhost:5000/api", // or change to your production URL
-//   withCredentials: false,
-// });
+// axios.defaults.baseURL = "http://localhost:5000/api";
+// axios.defaults.withCredentials = true;
 
-// // Add token to every request
-// axiosInstance.interceptors.request.use((config) => {
-//   const token = localStorage.getItem("token");
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// }, (error) => {
-//   return Promise.reject(error);
-// });
-
-// export default axiosInstance;
-
-// // --- Tailwind Utility ---
-// import { clsx } from "clsx";
-// import { twMerge } from "tailwind-merge";
-
-// export function cn(...inputs) {
-//   return twMerge(clsx(inputs));
-// }
-
-// lib/utils.js
-
-// export const cn = (...classes) => {
-//   return classes.filter(Boolean).join(" ");
+// // ✅ Fetch wishlist
+// export const fetchWishlist = async () => {
+//   const res = await axios.get("/wishlist");
+//   return res.data;
 // };
 
-// // LocalStorage Token Utils
-// export const getToken = () => {
-//   if (typeof window !== "undefined") {
-//     return localStorage.getItem("token");
-//   }
-//   return null;
+// // ✅ Add to wishlist
+// export const addToWishlist = async (productId) => {
+//   const res = await axios.post("/wishlist", { productId });
+//   return res.data;
 // };
 
-// export const saveToken = (token) => {
-//   if (typeof window !== "undefined") {
-//     localStorage.setItem("token", token);
-//   }
-// };
-
-// export const removeToken = () => {
-//   if (typeof window !== "undefined") {
-//     localStorage.removeItem("token");
-//   }
-// };
-
-// export const getToken = () => {
-//   const tokenFromStorage = localStorage.getItem("token");
-//   if (tokenFromStorage) return tokenFromStorage;
-
-//   // Try from cookies if not in localStorage
-//   const match = document.cookie.match(/token=([^;]+)/);
-//   return match ? match[1] : null;
+// // ✅ Remove from wishlist
+// export const removeFromWishlist = async (productId) => {
+//   const res = await axios.delete(`/wishlist/${productId}`);
+//   return res.data;
 // };
